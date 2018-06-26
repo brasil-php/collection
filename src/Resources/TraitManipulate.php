a0257f3159b6542bb431fb2703b3a2a9261c1fde
William Correa <wilcorrea@gmail.com>
2018-06-26 19:19:57 -0300

    Add methods like append and prepend to manipulate current

--- src/Resources/TraitManipulate.php
+++ src/Resources/TraitManipulate.php
@@ -14,8 +14,7 @@ trait TraitManipulate
      */
     public function push($record)
     {
-        array_push($this->records, $record);
-        return $this->length();
+        return array_push($this->records, $record);
     }
 
     /**
@@ -24,5 +23,42 @@ trait TraitManipulate
     public function pop()
     {
         return array_pop($this->records);
+    }
+
+    /**
+     * @param mixed $record
+     * @return int
+     */
+    public function unShift($record)
+    {
+        return array_unshift($this->records, $record);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function shift()
+    {
+        return array_shift($this->records);
+    }
+
+    /**
+     * @param array $array
+     * @return int
+     */
+    public function append(array $array)
+    {
+        $this->records = array_merge( $array, $this->records);
+        return $this->length();
+    }
+
+    /**
+     * @param array $array
+     * @return int
+     */
+    public function prepend(array $array)
+    {
+        $this->records = array_merge(  $this->records, $array);
+        return $this->length();
     }
 }
